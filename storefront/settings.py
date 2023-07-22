8006716f5ca3b57d39e1002a90fc193e8bdf0b72
manchur <manchur.fiverr@gmail.com>
2023-07-22 10:15:38 +0600

    working with CORS

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
     'debug_toolbar',
+
     'store',
     'tags',
     'likes',
@@ -50,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -64,6 +68,11 @@ INTERNAL_IPS = [
     # ...
     '127.0.0.1',
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
 ]
 
 ROOT_URLCONF = 'storefront.urls'
